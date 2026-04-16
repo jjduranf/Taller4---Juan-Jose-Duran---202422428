e2a55b86bfffd91b30665fe301af0b32ca044a82
Juan Duran <jj.duranf1@uniandes.edu.co>
2026-04-15 21:58:52 -0500

    Parte 2

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
-import { Serie } from "./serie";
-import { series } from "./data";
+import { Serie } from "./serie.js";
+import { series } from "./data.js";
 
 let seriesTbody: HTMLElement = document.getElementById('series')!;
 let promSeasons: HTMLElement = document.getElementById('prom-seasons')!;
+let serieName: HTMLElement = document.getElementById('serie-name')!;
+let serieChannel: HTMLElement = document.getElementById('serie-channel')!;
+let serieSeasons: HTMLElement = document.getElementById('serie-seasons')!;
+let serieDescription: HTMLElement = document.getElementById('serie-description')!;
+let serieLink: HTMLElement = document.getElementById('serie-link')!;
+let seriePoster: HTMLElement = document.getElementById('poster')!;
 
 renderSeriesInTable(series);
 
@@ -18,6 +24,10 @@ function renderSeriesInTable(series: Serie[]): void {
                             <td>${serie.name}</td>
                             <td>${serie.channel}</td>
                             <td>${serie.seasons}</td>`;
+    trElement.addEventListener('click', () => {
+            updateCardDetails(serie);
+        });
+
     seriesTbody.appendChild(trElement);
   });
 }
@@ -31,3 +41,15 @@ function mostrarPromedioSeasons(promedio: number): void {
     promSeasons.textContent = `Seasons average: ${promedio}`;
 }
 
+function updateCardDetails(serie: Serie): void {
+    const imgElement = seriePoster as HTMLImageElement;
+    
+
+    imgElement.src = serie.poster as string; 
+    imgElement.alt = `${serie.name} Poster`;
+    serieName.textContent = `Nombre: ${serie.name}`;
+    serieDescription.textContent = `Descripción: ${serie.description}`;
+    serieChannel.textContent = `Canal: ${serie.channel}`;
+    serieSeasons.textContent = `Temporadas: ${serie.seasons}`;
+    serieLink.innerHTML = `<a href="${serie.webpage}" class="btn btn-primary" target="_blank">Ver más</a>`;
+}
--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,12 @@ import { Serie } from "./serie.js";
 import { series } from "./data.js";
 var seriesTbody = document.getElementById('series');
 var promSeasons = document.getElementById('prom-seasons');
+var serieName = document.getElementById('serie-name');
+var serieChannel = document.getElementById('serie-channel');
+var serieSeasons = document.getElementById('serie-seasons');
+var serieDescription = document.getElementById('serie-description');
+var serieLink = document.getElementById('serie-link');
+var seriePoster = document.getElementById('poster');
 renderSeriesInTable(series);
 var averageSeasons = calcularPromedioSeasons(series);
 mostrarPromedioSeasons(averageSeasons);
@@ -11,6 +17,9 @@ function renderSeriesInTable(series) {
     series.forEach(function (serie) {
         var trElement = document.createElement("tr");
         trElement.innerHTML = " <td>".concat(serie.id, "</td>\n                            <td>").concat(serie.name, "</td>\n                            <td>").concat(serie.channel, "</td>\n                            <td>").concat(serie.seasons, "</td>");
+        trElement.addEventListener('click', function () {
+            updateCardDetails(serie);
+        });
         seriesTbody.appendChild(trElement);
     });
 }
@@ -20,5 +29,15 @@ function calcularPromedioSeasons(series) {
 }
 function mostrarPromedioSeasons(promedio) {
     promSeasons.textContent = "Seasons average: ".concat(promedio);
+}
+function updateCardDetails(serie) {
+    var imgElement = seriePoster;
+    imgElement.src = serie.poster;
+    imgElement.alt = "".concat(serie.name, " Poster");
+    serieName.textContent = "Nombre: ".concat(serie.name);
+    serieDescription.textContent = "Descripci\u00F3n: ".concat(serie.description);
+    serieChannel.textContent = "Canal: ".concat(serie.channel);
+    serieSeasons.textContent = "Temporadas: ".concat(serie.seasons);
+    serieLink.innerHTML = "<a href=\"".concat(serie.webpage, "\" class=\"btn btn-primary\" target=\"_blank\">Ver m\u00E1s</a>");
 }
 //# sourceMappingURL=main.js.map
